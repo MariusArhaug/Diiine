4c1dd64bf9430d23813c5025ecbe912fb2eead2f
Torunn Seyffarth <torunnse@stud.ntnu.no>
2021-02-10 14:28:19 +0100

    feat(21): add package lock and placeholder

--- client/middagsdeling_app/src/components/Login.tsx
+++ client/middagsdeling_app/src/components/Login.tsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import { Avatar } from '@material-ui/core';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
+
 
 export default class Login extends Component<{}, {}> {
 
@@ -17,10 +20,11 @@ export default class Login extends Component<{}, {}> {
 
     render() {
         return <div>
-            <h1>Login</h1>
-            <TextField label="Username" />
+            <h1>Sign in</h1>
+            <Avatar><PersonAddIcon/></Avatar>
+            <TextField label="Username" placeholder ='Enter username'/>
             <br/>
-            <TextField label="Password" />
+            <TextField label="Password" placeholder ='Enter password' />
             <br/>
             <Button onClick={this.login} variant="contained" color="primary">
                 Log in
