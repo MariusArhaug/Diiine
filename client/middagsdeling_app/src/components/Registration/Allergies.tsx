3bc5ba4b0b26f3b87b35c638c172b26eaa8b0f8a
larstonder <lars.tonder@outlook.com>
2021-02-15 12:59:26 +0100

    fix(#21): add package.json to gitinclude

--- client/middagsdeling_app/src/components/Registration/Allergies.tsx
+++ client/middagsdeling_app/src/components/Registration/Allergies.tsx
@@ -1,8 +1,6 @@
 import Checkbox from '@material-ui/core/Checkbox';
-import FormControl from '@material-ui/core/FormControl';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormGroup from '@material-ui/core/FormGroup';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import FormLabel from '@material-ui/core/FormLabel';
 import React from 'react';
 import { useStyles } from '../../styles';
