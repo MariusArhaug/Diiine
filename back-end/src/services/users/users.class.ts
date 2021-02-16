000f4d8fc327ca38391a921177c360588323b187
karenhompland <khompland@gmail.com>
2021-02-16 13:54:26 +0100

    prøving av create, commit for å lagre/dele

--- back-end/src/services/users/users.class.ts
+++ back-end/src/services/users/users.class.ts
@@ -1,34 +1,74 @@
-import { Service, KnexServiceOptions } from 'feathers-knex';
+//import { Service, KnexServiceOptions } from 'feathers-knex';
 import { Application } from '../../declarations';
+import { Params } from '@feathersjs/feathers';
 
-export class Users extends Service {
+//let allergensMap = new Map();
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host  : 'mysql.stud.ntnu.no',
+  user  : 'fs_tdt4140_1_gruppe40',
+  password: 'vielskerPUlol40',
+  database: 'fs_tdt4140_1_gruppe40_mddb'
+})
+
+/*connection.connect( (err: any) => {
+  if (err) throw err;
+  connection.query(`INSERT INTO users VALUES ()`, (err: any, results: any, fields: any) => {
+    if (err) throw err;
+      console.log("funker:)")
+      console.log(results[0])
+  })
+})
+ */
+// Type interface for user
+interface UserData {
+  userId?: string;
+  email: string;
+  passwordHash: string;
+  name?: string;
+  allergens?: Map<string, boolean>;
+}
+
+export class createUser {
+  
+  /* const mysql = require('mysql');
+  const connection = this.mysql.createConnection({
+    host  : 'mysql.stud.ntnu.no',
+    user  : 'fs_tdt4140_1_gruppe40',
+    password: 'vielskerPUlol40',
+    database: 'fs_tdt4140_1_gruppe40_mddb'
+  }) */
+/*   this.connection.connect( (err: any) => {
+    if (err) throw err;
+    connection.query(`INSERT INTO users VALUES ("userID", name, email, password, created_at, updated_at, is_admin, allergens)`, (err: any, results: any, fields: any) => {
+      if (err) throw err;
+        console.log("funker:)")
+        console.log(results[0])
+    })
+  })   */
+}
+
+/* export class Users extends Service {
   //eslint-disable-next-line @typescript-eslint/no-unused-vars
   constructor(options: Partial<KnexServiceOptions>, app: Application) {
     super({
       ...options,
       name: 'users'
-    });
+    });}
+  
+  create (data: UserData, params?: Params) {
+    const { email, passwordHash, name, allergens } = data;
+    const userData = {
+      email,
+      passwordHash,
+      name,
+      allergens
+      };
+
+    // Call the original `create` method with existing `params` and new data
+    return super.create(userData, params);
+  } */
 
     // CRUD - Create, Read, Update, Delete.
-
-    /* {
-      user_id: 
-      email: 
-      password_hash:
-      allergener:
-
-      create(userObject) {
-        user_id = tildatabese.save(); 
-        ...
-
-      }
-
-      findAll() {
-        // admin hente ut en liste av brukere. 
-      }
-
-    } */
-
-
-  }
-}
+//}
--- back-end/src/services/users/users.hooks.ts
+++ back-end/src/services/users/users.hooks.ts
@@ -1,17 +1,26 @@
 import * as feathersAuthentication from '@feathersjs/authentication';
 import * as local from '@feathersjs/authentication-local';
+import { HookContext } from '@feathersjs/feathers';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const { authenticate } = feathersAuthentication.hooks;
 const { hashPassword, protect } = local.hooks;
+const createdAt = async (context: HookContext) => {
+  context.data.createdAt = new Date();
+  return context;
+};
+const updatedAt = async (context: HookContext) => {
+  context.data.updatedAt = new Date();
+  return context;
+};
 
 export default {
   before: {
     all: [],
     find: [ authenticate('jwt') ],
     get: [ authenticate('jwt') ],
-    create: [ hashPassword('password') ],
-    update: [ hashPassword('password'),  authenticate('jwt') ],
+    create: [ hashPassword('password'), createdAt ], //må evt endre til createUser
+    update: [ hashPassword('password'),  authenticate('jwt'), updatedAt ],
     patch: [ hashPassword('password'),  authenticate('jwt') ],
     remove: [ authenticate('jwt') ]
   },
