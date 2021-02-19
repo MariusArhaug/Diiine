2d7cffb536a8934298e41316848bba0828d17469
Schäffer <jnschaff@stud.ntnnu.no>
2021-02-19 13:28:13 +0100

    refactor hooks

---
+++ back-end/src/hooks/parse-object-to-string.ts
@@ -0,0 +1,16 @@
+// Use this hook to manipulate incoming or outgoing data.
+// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+import { Hook, HookContext } from '@feathersjs/feathers';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default (field: string): Hook => {
+  return async (context: HookContext): Promise<HookContext> => {
+    const { data } = context;
+
+    const result = Object.keys(data[field]).filter(key => data[field][key] === 1);
+    context.data[field] = result.join();
+
+    console.log(context.data[field]);
+    return context;
+  };
+};
