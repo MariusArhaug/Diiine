1c0122b95c28d9567fb2fdcf2bc7f5eecc91832f
Ivar Carlsen <ivarhc@stud.ntnu.no>
2021-02-22 12:59:48 +0100

    feat (#23): edit the dinnerlist

--- client/middagsdeling_app/src/components/DinnerList.tsx
+++ client/middagsdeling_app/src/components/DinnerList.tsx
@@ -3,6 +3,8 @@ import ListComponent from '../components/ListComponent';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper'
+import { Dinner } from '../types'
+import { User } from '../types'
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -13,6 +15,7 @@ const useStyles = makeStyles((theme: Theme) =>
             padding: theme.spacing(2),
             margin: 'auto',
             maxWidth: 10000,
+            backgroundColor: '#4d5398',
         },
         image: {
             width: 128,
@@ -27,23 +30,81 @@ const useStyles = makeStyles((theme: Theme) =>
     }),
 );
 
-export type Dinner = {
-    name: string;
-    description: string;
+const user1: User = {
+    userId: 10,
+    name: "Navn Navnesen",
+    address: "Moholt",
+    email: "mohotl@gmail.com",
+    isAdmin: false,
+   allergies: ["nuts", "milk"]
+}
+
+const user2: User = {
+    userId: 11,
+    name: "Hei Heisen",
+    address: "Berg",
+    email: "berg@gmail.com",
+    isAdmin: false,
+    allergies: []
+}
+
+const user3: User = {
+    userId: 12,
+    name: "Hade Hadeland",
+    address: "Lerkendal",
+    email: "lerkendal@gmail.com",
+    isAdmin: false,
+    allergies: ["Gluten"]
 }
 
 const dinner1: Dinner = {
+    dinnerId: 1,
+    owner: user1,
     name: "test",
-    description: "desc"
+    description: "Nå sitter jeg og ser på skiskyting og lurer på hvordan den reagerer dersom jeg skriver mye tekst, om det vil bli slikt at det bli fortsatt bare en stor og lang tekstblokk, eller om det hopper ned",
+    address: "Gloes",
+    type: "Meat",
+    allergens: "None",
+    attendants: [user2, user3],
+    date: new Date()
 }
 const dinner2: Dinner = {
+    dinnerId: 2,
+    owner: user2,
     name: "test2",
-    description: "desc2"
+    description: "desc2",
+    address: "Dragvoll",
+    type: "Vegan",
+    allergens: "Nut",
+    attendants: [user3],
+    date: new Date()
 }
 const dinner3: Dinner = {
+    dinnerId: 3,
+    owner: user3,
     name: "test3",
-    description: "desc3"
+    description: "desc3",
+    address: "Tunga",
+    type: "Fish",
+    allergens: "Gluten, nuts",
+    attendants: [],
+    date: new Date()
 }
+
+/*
+export type Rating = {
+    rated: User;
+    ratedBy: User;
+    description: string;
+    rating: number;
+}
+
+export type Chat = {
+    to: User;
+    from: User;
+    message: string;
+}
+*/
 
 const dinners: Dinner[] = [dinner1, dinner2, dinner3];
 
--- client/middagsdeling_app/src/components/ListComponent.tsx
+++ client/middagsdeling_app/src/components/ListComponent.tsx
@@ -1,28 +1,89 @@
+import {Grid} from '@material-ui/core';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
 import React from 'react';
 import { Dinner } from '../types';
+import { createMuiTheme } from '@material-ui/core/styles';
 
-export default function ListComponent(props : Dinner) {
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      margin: 'auto',
+      maxWidth: 500,
+      backgroundColor: "#a9afff"
+    },
+    image: {
+      width: 128,
+      height: 128,
+    },
+    img: {
+      margin: 'auto',
+      display: 'block',
+      maxWidth: '100%',
+      maxHeight: '100%',
+    },
+  }),
+);
+
+export default function ListComponent(props: Dinner) {
+
+    const classes = useStyles();
 
     return (
-        <Paper elevation={3} >
-            <p>{props.owner}: {props.description}</p>
+        <div className={classes.root}>
+        <Paper className={classes.paper}>
+          <Grid container spacing={2}>
+            <Grid item>
+              <p>{props.dinnerId}</p>
+            </Grid>
+            <Grid item xs={12} sm container>
+              <Grid item xs container direction="column" spacing={2}>
+                <Grid item xs>
+                  <Typography gutterBottom variant="subtitle1">
+                    {props.name}
+                  </Typography>
+                  <Typography variant="body2" gutterBottom>
+                    {props.address}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary">
+                    {props.allergens}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="body2" style={{ cursor: 'pointer' }}>
+                    Join
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle1">{props.type}</Typography>
+              </Grid>
+            </Grid>
+          </Grid>
         </Paper>
+      </div>
     );
+        /*
+        <Paper>
+            <p>Host: {props.owner.name} {props.description}</p>
+            <Grid>
+                <p>Location: {props.address}</p>
+                <Paper>
+                    <p>Address: {props.address}</p>
+                    
+                </Paper>
+            </Grid>
+        </Paper>
+        
+    );
+        */
 
-    /*
-        name
-        adress
-        type (veggi, meaty)
-        allergens
-        attendants
-        isDivided
-        isOpen
-        expenses
-        date
-        description
-        Se på types.ts for å se på hva som skal gjøres
-        Koble DinnerID opp til middag
-    */
 }
 
