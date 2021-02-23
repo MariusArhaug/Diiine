b234cf660827caa14a45b5e540d8f788d364b150
eskildha <eskild.g.hansen@gmail.com>
2021-02-23 15:20:22 +0100

    feat(#12): På- eller avmelding, nytt endpoint

--- back-end/src/services/attendingdinners/attendingdinners.class.ts
+++ back-end/src/services/attendingdinners/attendingdinners.class.ts
@@ -28,4 +28,16 @@ export class Attendingdinners extends Service {
 
     return super.create(attendingdinnersdata, params);
   }
+
+  /*
+
+  TODO: 
+  1. Restrict users from deleting entries
+  This might be handled in front-end, users should only be able to delete themselves from the signup-table, unless they are
+  the owner of the dinner or an admin.
+
+  2. Use other services to return lists of users or dinners based on queries. Ex. find dinners you are signed up for, or find 
+  a list of attendees.
+
+  */
 }
