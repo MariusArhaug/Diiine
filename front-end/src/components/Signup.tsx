c3993682c270a2cf5b30f96fccf8569b083fd472
Marius Arhaug <mariuhar@stud.ntnu>
2021-02-25 12:13:05 +0100

    wip: reverted route to

--- front-end/src/components/Signup.tsx
+++ front-end/src/components/Signup.tsx
@@ -151,7 +151,7 @@ export const Signup = () => {
                                 </Grid>
 
                                 <Grid item xs={12}>
-                                    <Button component={RouterLink} to="/profile" type='submit' variant="contained" color="primary" style={{ width: "100%" }}>
+                                    <Button type='submit' variant="contained" color="primary" style={{ width: "100%" }}>
                                         Sign Up
                                 </Button>
                                 </Grid>
@@ -166,6 +166,7 @@ export const Signup = () => {
                             </Grid>
                         </form>
                     </Paper>
+                    <Button component={RouterLink} to='/profile'>Dinners</Button>
                 </Container>
             </div>
         </div>
