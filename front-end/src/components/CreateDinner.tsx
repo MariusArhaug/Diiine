c90a50e57e1ebbcfee34f6c5a948fabb95dcf182
Marius Arhaug <mariuhar@stud.ntnu>
2021-02-25 11:16:18 +0100

    feat: added allergies

--- front-end/src/components/CreateDinner.tsx
+++ front-end/src/components/CreateDinner.tsx
@@ -14,7 +14,16 @@ import { useStyles } from '../styles';
 const allergies = [
     {label: 'Lactose', value: 'lactose'},
     {label: 'Gluten', value: 'gluten'},
-    {label: 'Nuts', value: 'nuts'},
+    {label: 'Shellfish', value: 'shellfish'},
+    {label: 'Egg', value: 'egg'},
+    {label: 'Fish', value: 'fish'},
+    {label: 'Mustard', value: 'mustard'},
+    {label: 'Celleri', value: 'celleri'},
+    {label: 'Peanuts', value: 'peanuts'},
+    {label: 'Soy', value: 'soy'},
+    {label: 'Molluscs', value: 'molluscs'},
+    {label: 'Lupin', value: 'lupin'},
+    {label: 'Sulfites', value: 'sulfites'},
 ]
 
 const tags = [
