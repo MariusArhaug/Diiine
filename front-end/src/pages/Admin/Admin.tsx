af77197737a510804bfa1903415a7036d1df8e62
Marius Arhaug <mariuhar@stud.ntnu>
2021-03-29 15:08:58 +0200

    remove(misc): remove unused fields

--- front-end/src/pages/Admin/Admin.tsx
+++ front-end/src/pages/Admin/Admin.tsx
@@ -4,6 +4,7 @@ import client from '../../feathers-client'
 import { Grid } from "@material-ui/core";
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import AdminCard from './AdminCard';
+import { useAuth } from '../../hooks/use-auth';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -14,28 +15,34 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 export default function Admin() {
-
+  const thisAdmin: User = useAuth().user;
   const classes = useStyles();
   const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
     client.service('users')
-      .find({})
+      .find({
+        query: {
+          user_id: {
+            $ne: thisAdmin.user_id,
+          }
+        }
+      })
       .then((res: any) => {
         const users: User[] = res.data;
         setUsers([...users]);
       })
       .catch((e: Error) => { console.log('error', e) })
-  }, []);
+  }, [thisAdmin.user_id]);
 
   return (
     <div className={classes.root}>
       <Grid container spacing={3} direction='column' justify='space-evenly' alignItems='stretch'>
-        {users.length && users!.map((user: User) => (
+        {users.length ? users!.map((user: User) => (
           <Grid item key={user.user_id}>
             <AdminCard {...user} key={user.user_id} />
           </Grid>
-        ))}
+        )) : null}
       </Grid>
     </div>
   )
--- front-end/src/pages/Admin/EditButton.tsx
+++ front-end/src/pages/Admin/EditButton.tsx
@@ -2,7 +2,7 @@ import React, { useCallback } from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { createMuiTheme } from '@material-ui/core/styles';
-import { Dinner } from '../../types';
+import { Dinner, User } from '../../types';
 import { Link as RouterLink, useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -16,14 +16,18 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-export default function EditButton(props: Dinner) {
+export default function EditButton(props: Dinner | User) {
   const history = useHistory();
-  const handleOnClick = useCallback(() => history.push('/editdinner/' + props.dinners_id), [history]);
+
+  let path = ''
+  if (props as Dinner) path = `/editdinner/${(props as Dinner).dinners_id}`;
+  else path = `/edituser/${(props as User).user_id}`;
+  const handleClick = useCallback(() => history.push(path), [path, history]);
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <Button classes={{root: classes.root}} onClick={handleOnClick}>Edit</Button>
+      <Button classes={{ root: classes.root }} onClick={handleClick}>Edit</Button>
     </div>
   );
 }
