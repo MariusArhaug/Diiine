ab7bd403894a66d8cd561b598ec7924446f7dedb
Marius Arhaug <mariuhar@stud.ntnu>
2021-04-01 18:56:28 +0200

    refactor(find-owner/attendant): now works with get method

--- back-end/src/hooks/find-attendants.ts
+++ back-end/src/hooks/find-attendants.ts
@@ -6,6 +6,27 @@ import { UserData } from '../services/users/users.class'
 import { DinnerData } from '../services/dinners/dinners.class'
 import { AttendingdinnersData } from '../services/attendingdinners/attendingdinners.class';
 
+async function findDinnersAttendants(dinners: DinnerData[]) {
+  return Promise.all(dinners
+    .map(async (dinner: DinnerData) => ({
+      ...dinner,
+      attendants: await getAttendants(dinner.dinners_id)
+    }))
+  )
+}
+
+async function getAttendants(id: number): Promise<UserData[]> {
+  return await Promise.resolve((app.service('attendingdinners')
+    .find({query: {dinners_id : id}})
+    .then(async (res: any) => await Promise.all(res.data
+      .map((row: AttendingdinnersData) => (
+        app.service('users')
+          .get(row.user_id)
+          .then((res: UserData) => res)
+      ))
+    ))
+  ))
+}
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default (options = {}): Hook => {
@@ -15,23 +36,18 @@ export default (options = {}): Hook => {
     if (!query) {
       throw new Error('Error! ')
     }
+    if (context.method === 'get') {
+      console.log(context.result)
+      context.result = {
+        ...context.result,
+        attendants: await getAttendants(context.result.dinners_id)
+      }
+      console.log(context.result)
+    } 
+    else {
+      context.result.data = await findDinnersAttendants(context.result.data)
+    }
     
-    context.result.data = await Promise.all(context.result.data
-      .map(async (dinners: DinnerData) => ({
-        ...dinners,
-        attendants: await Promise.resolve((app.service('attendingdinners')
-            .find({query: {dinners_id : dinners.dinners_id}})
-            .then(async (res: any) => await Promise.all(res.data
-              .map((row: AttendingdinnersData) => (
-                app.service('users')
-                  .get(row.user_id)
-                  .then((res: UserData) => res)
-              ))
-            ))
-          )) 
-      }))
-    )
-  
     return context;
   };
 };
